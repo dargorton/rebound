e1fa4e2c56ee062035824896653648cbabd48e7c
10trips <ethan.freckleton@gmail.com>
2024-11-12 20:30:05 -0600

    change version 3.1-nightly-1

--- src/main/java/net/backdoor/client/Backdoor.java
+++ src/main/java/net/backdoor/client/Backdoor.java
@@ -15,7 +15,7 @@ import net.minecraft.client.util.InputUtil;
 
 public class Backdoor implements ClientModInitializer {
 
-    public static final String VERSION = "3.0-nightly-5+" + MinecraftClient.getInstance().getGameVersion();
+    public static final String VERSION = "3.1-nightly-1+" + MinecraftClient.getInstance().getGameVersion();
     private KeyBinding keyBinding;
     public Screen currentScreen = MinecraftClient.getInstance().currentScreen;
 
